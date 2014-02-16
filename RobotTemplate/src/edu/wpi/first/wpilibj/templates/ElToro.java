a5fbc48dc041f89e65f141dc50e129d8c95b2ac2
artedeschi <artedeschi@gmail.com>
2014-02-15 17:04:30 -0800

    Added El Toro Class

---
+++ RobotTemplate/src/edu/wpi/first/wpilibj/templates/ElToro.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package edu.wpi.first.wpilibj.templates;
+
+import edu.wpi.first.wpilibj.Jaguar;
+import org.wvrobotics.util.Robot;
+
+/**
+ *
+ * @author Spaceguy44
+ */
+public class ElToro {
+    
+}
