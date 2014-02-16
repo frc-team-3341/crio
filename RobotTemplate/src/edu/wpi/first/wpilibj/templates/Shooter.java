447b309ee27e0339b4a592212577f5b96a696fce
AgentG010 <georgetroulis@gmail.com>
2014-02-15 16:30:50 -0800

    Removed loader from shooter class
    the loader(el toro) will have its own individual class

--- RobotTemplate/src/edu/wpi/first/wpilibj/templates/Shooter.java
+++ RobotTemplate/src/edu/wpi/first/wpilibj/templates/Shooter.java
@@ -14,42 +14,15 @@ import org.wvrobotics.util.Robot;
  * @author michael
  */
 public class Shooter {
-    private final int SHOOTER_PORT = 3;
-    private final int LOADER_PORT = 4;
+    private final int SHOOTER_PORT = 5;
     private Jaguar launcher;
-    private Jaguar loader;
     
     public Shooter(){
         launcher = new Jaguar(SHOOTER_PORT);
-        loader = new Jaguar(LOADER_PORT);
     }
     
     public void shoot(){
-        
-    }
-    
-    public void shoot(int millis){
-        launcher.set(1.0);
-        try {
-            Thread.sleep(millis);
-        } catch (InterruptedException ex) {
-        }
-        launcher.set(0);
-    }    
-    
-    public void shootClose(){
-        shoot(500);
-    }
-    
-    public void shootMedium(){
-        shoot(1000);
-    }
-    public void shootFar(){
-        shoot(1500);
-    }
-    
-    public void load(){
-        loader.set(0.5);
+        launcher.set(1.0);        
     }
     
     public void stop(){
