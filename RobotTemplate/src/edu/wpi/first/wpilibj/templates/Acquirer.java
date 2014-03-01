6a0dbb91fd25e0e10136337ec1035e7f889a1820
AgentG010 <georgetroulis@gmail.com>
2014-03-01 09:18:21 -0800

    Created javadoc for Acquirer class
    Ignore all the files

--- RobotTemplate/src/edu/wpi/first/wpilibj/templates/Acquirer.java
+++ RobotTemplate/src/edu/wpi/first/wpilibj/templates/Acquirer.java
@@ -1,54 +1,71 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package edu.wpi.first.wpilibj.templates;
 
 import edu.wpi.first.wpilibj.Jaguar;
 import edu.wpi.first.wpilibj.Talon;
 
 /**
- *
- * @author Spaceguy44 (Adam)
+ * @author Spaceguy44 (Adam Tedeschi)
  */
 public class Acquirer {
 
     private Jaguar vandoor;
-    private Talon acquirer1;
-    private Talon acquirer2;
+    private Talon acquirer_left;
+    private Talon acquirer_right;
     
-    public Acquirer(int vanDoor, int acquirer_1, int acquirer_2) {
+    /**
+     * Initializes the motors of the acquirer system.
+     * @param vanDoor The PWM port of the Van Door motor.
+     * @param acquirer_left The PWM port of the left acquirer motor.
+     * @param acquirer_right The PWM port of the right acquirer motor
+     */
+    public Acquirer(int vanDoor, int acquirer_left, int acquirer_right) {
         vandoor = new Jaguar(vanDoor);
-        acquirer1 = new Talon(acquirer_1);
-        acquirer2 = new Talon(acquirer_2);
+        this.acquirer_left = new Talon(acquirer_left);
+        this.acquirer_right = new Talon(acquirer_right);
     }
     
+    /**
+     * Moves the acquirer towards the robot.
+     */
     public void pitch_up () {
         vandoor.set(0.5);  
     }
     
+    /**
+     * Moves the acquirer away from the robot.
+     */
     public void pitch_down () {
         vandoor.set(-0.5);
     }
     
+    /**
+     * Stops the Van Door motor from moving completely.
+     */
     public void pitch_stop() {
         vandoor.set(0.0);
     }
     
+    /**
+     * Moves the acquirer motors in opposite directions to collect the ball.
+     */
     public void collect() {
-        acquirer1.set(0.5);
-        acquirer2.set(-0.5);
+        acquirer_left.set(0.5);
+        acquirer_right.set(-0.5);
     }
     
+    /**
+     * Moves the acquirer motors in opposite directions to unload the ball.
+     */
     public void dump() {
-        acquirer1.set(-0.5);
-        acquirer2.set(0.5);
+        acquirer_left.set(-0.5);
+        acquirer_right.set(0.5);
     }
     
+    /**
+     * Stops the acquirer motors from moving completely.
+     */
     public void acquirer_stop() {
-        acquirer1.set(0.0);
-        acquirer2.set(0.0);
+        acquirer_left.set(0.0);
+        acquirer_right.set(0.0);
     }    
 }
