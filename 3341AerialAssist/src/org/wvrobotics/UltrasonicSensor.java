600d6882c8f1f25b3336ed821fe16722175a2efe
George Troulis <georgetroulis@gmail.com>
2014-03-13 16:14:48 -0700

    Added getAverageDistance() for ultrasonic sensor
    It gets 12 values from the ultrasonic sensor, throws out the 2 extremes, and calculates the average from the remaining values.

--- 3341AerialAssist/src/org/wvrobotics/UltrasonicSensor.java
+++ 3341AerialAssist/src/org/wvrobotics/UltrasonicSensor.java
@@ -20,6 +20,25 @@ public class UltrasonicSensor {
         return ultrasonic.getVoltage() * voltageToInches;
     }
     
+    //take an average of 10 values
+    //measure 12 values and throw out the 2 extremes
+    public double getAverageDistance() 
+    {
+        double values[] = new int[12];
+        
+        for(int i = 0; i < 12; i++)
+            values[i] = getDistance();
+            
+        Array.sort(values);//sort in numeric order
+        
+        double sum;
+        for(int i = 1; i <= 10; i++) //don't use the 2 extreme values to calculate the average
+            sum += values[i];
+            
+        double average = sum / 10;
+        return average;
+    }
+    
     public String toString()
     {
         return Double.toString(getDistance());
